f55aa27bbaa817f8444eedf8c111331aa84764db
ParkhomenkoDV <parkho.m.enko@mail.ru>
2024-07-15 16:37:42 +0300

    add imshow

--- cv_tools.py
+++ cv_tools.py
@@ -5,3 +5,12 @@ import cv2 as cv
 """
 добавить функции из Нетологии cv!!!
 """
+
+
+def imshow(img, **kwargs):
+    """Демонстрация изображения при помощи matplotlib"""
+    plt.figure(figsize=kwargs.get('figsize', (12, 12)))
+    plt.imshow(img.numpy().astype("uint8"))
+    plt.title(kwargs.get('title', 'image'))
+    plt.axis("off")
+    plt.show()
